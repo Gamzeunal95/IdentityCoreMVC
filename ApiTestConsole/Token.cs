8147cf04186bbadd91aef4d43a21c868b546ee06
gamzeunal95 <gamzeunal9505@gmail.com>
2023-01-09 18:39:01 +0300

    Http client bearer authentication

---
+++ ApiTestConsole/Token.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace ApiTestConsole
+{
+    public class Token
+    { // token ile alakalı propertylerini yazıyoruz Postmande token aldığımızda verdiği özelliklere göre burada aynı yazım şekli ile belirttik.
+        [JsonProperty("accessToken")]
+        public string AccessToken { get; set; }
+
+
+        [JsonProperty("expiration")]
+        public string Expiration { get; set; }
+
+
+        [JsonProperty("refreshToken")]
+        public string RefreshToken { get; set; }
+
+    }
+}
